5f75c137e795eb534a84964f5bb3071ae57f8e47
ruthersmit B <ruthersmith@neotechdesign.com>
2020-01-08 12:33:53 -0500

    the days for sunday and saturday are properly aligned yet the weekdays are not

--- public/javascripts/Calendar.js
+++ public/javascripts/Calendar.js
@@ -19,27 +19,39 @@ class Calendar{
     displayDayTitle(){
         this.appendCalenderCell( this.getKeyByValue(this.days,0) )
         for(let col = 1; col < 6; col++){
-            this.appendCalenderCell(this.getKeyByValue(this.days,col),"<div class = 'col-md-2 calendar-cell" );
+            this.appendCalenderCell(this.getKeyByValue(this.days,col),true);
         }
         this.appendCalenderCell( this.getKeyByValue(this.days,6) )
     }
 
     displayDays(){
         let day_count = 1;
-        let openingDiv = "<div class = 'col-md-1 calendar-cell' " ;
+        
         for (let i = 0; day_count <= this.maxDays; i++){
+            let column2 = true;
 
-          if (i < this.firstDay){
-            // visibility: hidden;
-            this.calendarDiv.append(openingDiv + "style=' '>" + '.' + "</div>");
-          }else {
-            this.calendarDiv.append("<div class = 'col-md-1 calendar-cell'> "+ day_count + "</div>");
-            day_count++;
-          }
+            if(i % 7 == 0 || i  % 7 == 1){
+                column2 = false;
+            }
+
+            if (i < this.firstDay){
+                this.appendCalenderCell(".",column2, "' style =  'visibility: hidden'");
+             }else {
+                this.appendCalenderCell(day_count,column2);
+                day_count++;
+            }
         }
     }
-    appendCalenderCell(value,openingDiv = "<div class = 'col-md-1 calendar-cell" ){
-        this.calendarDiv.append(openingDiv +"' > "+ value + "</div>");
+    
+    appendCalenderCell(value, column2 = false, style = "' style = '' "){
+        let openingDiv1 = "<div class = 'col-md-1  text-right" + style;
+        let openingDiv2 = "<div class = 'col-md-2  text-right" + style ;
+
+        if(column2){
+            this.calendarDiv.append(openingDiv2 +"' > "+ value + "</div>");
+        }else{
+            this.calendarDiv.append(openingDiv1 +"' > "+ value + "</div>");
+        }
     }
 
      getKeyByValue(object, value) {
